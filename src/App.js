23e43bd5d82a13049e75c6531ef912d5ed24906d
Paweł <84644073+Pawel-Lechki@users.noreply.github.com>
2023-08-07 22:34:14 +0200

    add hero component

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 import { Topbar } from "./components/Topbar";
 
@@ -6,6 +7,7 @@ function App() {
     <div>
       <Topbar />
       <Navbar />
+      <Hero />
     </div>
   );
 }
